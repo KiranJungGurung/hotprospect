0968301563224d35ef49c356e8882d550d4b6c86
KiranJungGurung <kirangurung394@gmail.com>
2023-02-28 06:09:55 +0545

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Kiran Gurung on 28/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
